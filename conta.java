3ea762902cbc757dcbed88af41e2e9d81b26857d
daniloioio <fogaca.dan@gmail.com>
2022-05-20 15:17:10 -0300

    conta

---
+++ conta.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 5;
+                int c = 2;
+		System.out.println("hello world" + (c*(a+b));
+	}
+
+	}
+
+
